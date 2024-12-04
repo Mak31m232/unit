27f4cc6bf40ea6e4f54c2a1b29004901f797d970
Mak31m232 <max.lebedev1104@gmail.com>
2024-12-04 14:08:57 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+var shetmenu = 0;
+
+function menu() {
+    if (shetmenu==0) {
+        let menu = document.getElementById("menu") 
+        let menuicon = document.getElementById("icon-div") 
+        let registration = document.getElementById("registration")
+        registration.style.marginTop = "-33%"
+        menuicon.style.display="block"
+        menu.style.display="none"
+        shetmenu = 1;
+    }   
+    else if (shetmenu==1){
+        let boom = document.getElementById("menu")
+        let menuicon = document.getElementById("icon-div") 
+        let registration = document.getElementById("registration")
+        registration.style.marginTop = "-35%"
+        menuicon.style.display="none"
+        boom.style.display="block"
+        shetmenu = 0;
+    }
+}
+
+function registration() {
+	var registration = document.getElementById("registration")
+	registration.style.display = "block"
+}
+
+function registration2() {
+	var registration = document.getElementById("registration");
+	registration.style.display = "none"
+    var sign = document.getElementById("btn-sign");
+    var acaunt = document.getElementById("acaunt")
+    var creation = document.getElementById("creation")
+    var user = document.getElementById("usernameFiel").value
+    var user2 = document.getElementById("usernameField").value
+    console.log("Password: "+user)
+    console.log("Name: "+user2)
+    acaunt.style.marginLeft = "96.5%"
+    creation.style.marginLeft = "-46%"
+    sign.style.display = "none"
+    alert("Вы успешно вошли в акаунт")
+}
+
+function registration3() {
+	var registration = document.getElementById("registration");
+    var sign = document.getElementById("btn-sign");
+    var acaunt = document.getElementById("acaunt")
+    var creation = document.getElementById("creation")
+    var user = document.getElementById("usernameFiel").value
+    var user2 = document.getElementById("usernameField").value
+    console.log("Password: "+user)
+    console.log("Name: "+user2)
+    acaunt.style.marginLeft = "96.5%"
+    creation.style.marginLeft = "-46%"
+	sign.style.display = "none"
+    registration.style.display = "none"
+    alert("Вы успешно зарегистрировались")
+}
+
+function registration4() {
+	var f = document.getElementById("registration");
+	f.style.display = "none"
+}
+
+let shet = 0;
+
+function menu2() {
+    if (shet==0) {
+        let boom = document.getElementById("menu2")
+        boom.style.display="block"
+        shet = 1;
+    }   
+    else if (shet==1){
+        let boom = document.getElementById("menu2")
+        boom.style.display="none"
+        shet = 0;
+    }
+}
